eec5e853f08f089293aa0e22ac42c235c251640e
Cuong <cuongg1405@gmail.com>
2024-03-20 05:02:48 +0700

    feat(route): use payment route

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 const express = require("express");
-
-require("./configuration/EnvironmentVariable.js");
-const firestore = require("./persistency/Firestore.js");
+require("./configuration/index.js");
+const route = require("./route/index.js");
+const billService = require("./service/BillService.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
+app.use('/', route)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  const docRef = firestore.collection("Admin");
-  docRef.get().then((doc) => {
-    doc.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data());
-    });
-  });
 });
+
+// const test = async () => {
+//   console.log(await billService.getBillById('9IoqqFf90reDkFYgKWeo'))
+// }
+// test();
