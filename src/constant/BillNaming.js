6288a80a2b0c513dfa7a903976788bde3556c55e
Cuong <cuongg1405@gmail.com>
2024-03-20 04:56:26 +0700

    feat: add constant naming for bill collection

---
+++ src/constant/BillNaming.js
@@ -0,0 +1,12 @@
+const STRUCTURE = {
+  BILL: 'HoaDon',
+  ID: 'UID',
+  ADDRESS: 'diachi',
+  FULL_NAME: 'hoten',
+  ORDER_DATE: 'ngaydat',
+  PAYMENT_METHOD: 'phuongthuc',
+  PHONE: 'sdt',
+  TOTAL_AMOUNT: 'tongtien',
+  ORDER_STATUS: 'trangthai'
+}
+module.exports = STRUCTURE;
