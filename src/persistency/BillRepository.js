4837d91e337bdb7dd992c2587938db1607848c70
Cuong <cuongg1405@gmail.com>
2024-03-21 05:16:16 +0700

    feat(repos): add function to find and update bill

--- src/persistency/BillRepository.js
+++ src/persistency/BillRepository.js
@@ -8,9 +8,26 @@ const getAllBills = async () => {
     console.log(error);
   }
 };
+const getBillByTransactionCode = async (transactionCode) => {
+  const snapshot = await billDocRef.where("transactionCode", "==", transactionCode).get();
+  return snapshot.docs.map((doc) => {
+    const data = doc.data();
+    data.id = doc.id;
+    return data;
+  });
+}
 const getBillById = async (id) => {
   const snapshot = await billDocRef.doc(id).get();
   const data = snapshot.data();
   return data;
 };
-module.exports = { getAllBills, getBillById };
+const updateTransactionCode = async (billId, transactionCode) => {
+  // const result = await billDocRef.doc(billId).collection('transactions').add({transactionCode: transactionCode})
+  const result = await billDocRef.doc(billId).update({transactionCode: transactionCode});
+  return result;
+}
+const updatePaymentStatus = async (id, status) => {
+  const result = await billDocRef.doc(id).update({paymentStatus: status});
+  return result;
+}
+module.exports = { getAllBills, getBillById, updateTransactionCode, getBillByTransactionCode, updatePaymentStatus};
